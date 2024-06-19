9c8c1ef729ff8aea7b5028cfc86bda39537808a8
Harsh <harshchoprajpr@gmail.com>
2024-06-19 13:13:00 +0530

    data_library made

---
+++ main.py
@@ -0,0 +1,23 @@
+import pandas as pd 
+import matplotlib.pyplot as plt 
+
+
+from data_fetch import download_historical_data
+from performance import plot_data
+
+start_date = "2024-06-01"
+end_date = "2024-06-19"
+symbol = "RELIANCE.NS"
+
+
+# df = download_historical_data(symbol , start_date , end_date)
+x_axis = "Close"
+y_axis = "Date"  # we made date a column here 
+
+
+
+
+
+
+
+
---
+++ performance.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt 
+from data_fetch import download_historical_data
+
+def plot_data(symbol, x_axis, y_axis, start_date, end_date, timeframe="1d"):
+    # Download historical data
+    df = download_historical_data(symbol, start_date, end_date, timeframe)
+    
+    # Reset index to make 'Date' a column
+    df.reset_index(inplace=True)
+    
+    X = df[x_axis]
+    Y = df[y_axis]
+
+    plt.plot(X, Y)
+    plt.title(f'{y_axis} vs {x_axis} for {symbol}')
+    plt.xlabel(x_axis)
+    plt.ylabel(y_axis)
+    plt.grid(True)
+    plt.show()
+
+
+
+# start_date = "2024-06-01"
+# end_date = "2024-06-19"
+# symbol = "RELIANCE.NS"
+# x_axis = "Date" 
+# y_axis = "Open"
+
+plot_data(symbol, x_axis, y_axis, start_date, end_date,"1wk")
